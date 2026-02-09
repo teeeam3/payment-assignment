b2923aaf6c15bd13b75ead479021c513c723df32
DEV.HYUCK <wwh3218@gmail.com>
2026-02-09 10:18:37 +0900

    fix: createdAt 자동 생성 null값 지급 수정 완료

--- src/main/java/sparta/paymentassignment/common/entity/BaseEntity.java
+++ src/main/java/sparta/paymentassignment/common/entity/BaseEntity.java
@@ -2,6 +2,7 @@ package sparta.paymentassignment.common.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 
 @Getter
 @EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
 public class BaseEntity {
 
     @CreatedDate
