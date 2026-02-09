a2d6e4fcc90c5efbb759d069c148247b84d96cc9
DEV.HYUCK <wwh3218@gmail.com>
2026-02-09 10:14:37 +0900

    fix: createdAt 자동 생성 null값 지급 버그 수정

--- src/main/java/sparta/paymentassignment/common/entity/BaseEntity.java
+++ src/main/java/sparta/paymentassignment/common/entity/BaseEntity.java
@@ -3,6 +3,7 @@ package sparta.paymentassignment.common.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -11,7 +12,8 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
-    @Column(updatable = false)
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
     private LocalDateTime createdAt;
 
 }
